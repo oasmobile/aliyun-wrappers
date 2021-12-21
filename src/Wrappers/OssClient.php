25c5ca49b8d5dbc8caca220a251cfb554bfbfb40
qitao.zhang <zhangqitao@oasgames.com>
2021-12-21 10:52:46 +0800

    fix adb manifest path

--- src/Wrappers/OssClient.php
+++ src/Wrappers/OssClient.php
@@ -7,10 +7,12 @@ use OSS\Core\OssException;
 
 class OssClient extends \OSS\OssClient
 {
-
+    
+    
     public function getPresignedUri($path, $expires = '+30 minutes')
     {
-
+        $path = $this->normalizeFilePath($path);
+        
         if (preg_match('#^oss://(.*?)/(.*)$#', $path, $matches)) {
             $bucket = $matches[1];
             $path   = $matches[2];
@@ -18,15 +20,33 @@ class OssClient extends \OSS\OssClient
         else {
             throw new InvalidArgumentException("path should be a full path starting with oss://");
         }
-
-        echo $timeout = is_string($expires) ? strtotime($expires) - time() : $expires;
-
+        
+        $timeout = is_string($expires) ? strtotime($expires) - time() : $expires;
+        
         try {
             return $this->signUrl($bucket, $path, $timeout);
         }
         catch (OssException $e) {
         }
-
+        
         return  false;
+    }
+    private function normalizeFilePath($path)
+    {
+        
+        static $protocol = "oss://";
+        if ($this->stringStartsWith($path, $protocol)) {
+            $path = substr($path, strlen($protocol));
+        }
+        $path = preg_replace('#/+#', "/", $path);
+        
+        return $protocol . $path;
+    }
+    private function stringStartsWith($haystack, $needle)
+    {
+        
+        return
+            $needle === ""
+            || strrpos($haystack, $needle, -strlen($haystack)) !== false;
     }
 }
